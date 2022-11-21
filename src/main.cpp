be78874882ab0eff43edf39835fc89dd9439bdc2
t0a5ted <flameburstunstoppable@gmail.com>
2022-11-21 23:00:52 +0800

    feat(decompress): reconstruct tree and text data, include text length in compressed file

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,9 @@ struct HuffTreeNodeCompare {
     }
 };
 
+typedef std::unordered_map<char, std::string>  tree_char_path_map_t;
+typedef std::unordered_map<std::string, char>  tree_path_char_map_t;
+
 struct BitWriter {
     std::ofstream& out_fhandler;
     unsigned char output_byte = 0;
@@ -30,7 +33,6 @@ struct BitWriter {
     BitWriter(std::ofstream& out_f) : out_fhandler(out_f) {}
 
     void write_bit(unsigned char bit) {
-        std::cout << (int) bit << std::endl;
         output_byte <<= 1;
         if (bit) output_byte |= 1;
         bit_count++;
@@ -43,6 +45,12 @@ struct BitWriter {
     void write_char(char byte) {
         for (int bit=7; bit>=0; bit--) {
             write_bit((byte >> bit) & 0x01);
+        }
+    }
+
+    void write_int(int n) {
+        for (int bit=31; bit>=0; bit--) {
+            write_bit((n >> bit) & 0x01);            
         }
     }
 
@@ -59,6 +67,10 @@ struct BitReader {
 
     BitReader(std::ifstream& in_f) : in_fhandler(in_f) {}
 
+    unsigned int peek_bit() {
+        return (input_byte >> (7-bit_count)) & 0x01;
+    }
+
     unsigned int read_bit() {
         // def could be refactored more simply
         if (bit_count == 8) // last bit has been read, reset bit_count
@@ -69,9 +81,27 @@ struct BitReader {
             if (input_byte == EOF)
                 return 2;
         } 
-        unsigned int b = (input_byte >> (7-bit_count)) & 0x01;
+        unsigned int b = peek_bit();
         bit_count++;
         return b;
+    }   
+
+    char read_char() {
+        char c;
+        for (int i=7; i>=0; i--) {
+            c <<= 1;
+            if (read_bit()) c |= 1;
+        }
+        return c;
+    }
+
+    int read_int() {
+        int n;
+        for (int i=31; i>=0; i--) {
+            n <<= 1;
+            if (read_bit()) n |= 1;
+        }
+        return n;
     }
 };
 
@@ -86,7 +116,7 @@ void write_hufftree(BitWriter *bw, HuffTreeNode* root) {
     write_hufftree(bw, root->right);
 }
 
-void populate_tree_path_map(std::unordered_map<char, std::string>* map, HuffTreeNode* root, std::string path) {
+void populate_tree_path_map(tree_char_path_map_t* map, HuffTreeNode* root, std::string path) {
     if (root->ascii_char != '\0') {
         (*map)[root->ascii_char] = path;
         return;
@@ -95,7 +125,7 @@ void populate_tree_path_map(std::unordered_map<char, std::string>* map, HuffTree
     populate_tree_path_map(map, root->right, path+'1');
 }
 
-void write_textdata(BitWriter* bw, std::unordered_map<char, std::string>* tree_map, std::ifstream& in_stream) {
+void write_textdata(BitWriter* bw, tree_char_path_map_t* tree_map, std::ifstream& in_stream) {
     in_stream.seekg(0);
     while (in_stream.peek() != EOF) {
         std::string path = (*tree_map)[in_stream.get()];
@@ -104,24 +134,49 @@ void write_textdata(BitWriter* bw, std::unordered_map<char, std::string>* tree_m
     }
 }
 
+void construct_tree_path_map_from_bits(BitReader* br, tree_path_char_map_t* m, std::string curr_path) {
+    unsigned int node_type = br->read_bit();
+    if (node_type == 0) {
+        construct_tree_path_map_from_bits(br, m, curr_path+'0');
+        construct_tree_path_map_from_bits(br, m, curr_path+'1');        
+    } else if (node_type == 1) {
+        (*m)[curr_path] = br->read_char();
+    } else {
+        throw std::runtime_error("Invalid compressed format: no more bits left to read");
+    }
+}
 
 void decompress(std::ifstream &in_fhandler, std::ofstream &out_fhandler) {
     // construct huff tree
     BitReader* br = new BitReader(in_fhandler);
-    unsigned int bit;
-    while (bit != 2) {
+    int text_length = br->read_int();
+    tree_path_char_map_t* map = new tree_path_char_map_t();
+    construct_tree_path_map_from_bits(br, map, "");
+
+    // uncompress text data
+    unsigned int bit = br->read_bit();
+    std::string path;
+    while (bit != 2 && text_length) {
+        char c = bit + '0';
+        path += c;
+        if (map->count(path)) {
+            out_fhandler << (*map)[path];
+            text_length--;
+            path.clear();
+        }
         bit = br->read_bit();
-        std::cout << bit << std::endl;
     }
 }
 
 void compress(std::ifstream &in_fhandler, std::ofstream &out_fhandler) {
-    // construct freq table
+    // construct freq table and get text length
     std::unordered_map<char, int> freq_table; 
+    int text_length = 0;
     in_fhandler.seekg(0);
     while (in_fhandler.peek() != EOF) {
         char c = in_fhandler.get();
         freq_table[c]++;
+        text_length++;
     }
     // populate min heap
     std::priority_queue<HuffTreeNode*, std::vector<HuffTreeNode*>, HuffTreeNodeCompare> q;    
@@ -140,10 +195,11 @@ void compress(std::ifstream &in_fhandler, std::ofstream &out_fhandler) {
         q.push(new_node);
     }
     // construct char to tree path map
-    std::unordered_map<char, std::string>* tree_map = new std::unordered_map<char, std::string>();
+    tree_char_path_map_t* tree_map = new tree_char_path_map_t();
     populate_tree_path_map(tree_map, q.top(), "");
     // write tree to file
     BitWriter *bw = new BitWriter(out_fhandler);
+    bw->write_int(text_length);
     write_hufftree(bw, q.top());
     write_textdata(bw, tree_map, in_fhandler);
     bw->flush();
